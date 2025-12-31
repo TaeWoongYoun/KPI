d8af05bbf8b7f2acfe7a7edd0fe47faa92bb0c7f
TaeWoongYoun <24457545yong@gmail.com>
2025-12-31 14:08:01 +0900

    test : First commit

---
+++ src/test/java/com/example/KPI/KpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
